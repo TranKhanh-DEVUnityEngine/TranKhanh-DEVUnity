3b53b03777fbc5d213267637ece0eadf567b689f
TranKhanh-DEVUnityEngine <93987352+TranKhanh-DEVUnityEngine@users.noreply.github.com>
2021-12-20 05:02:20 -0800

    first commit

---
+++ sapxepmang/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sapxepmang
+{
+    class Class1
+    {
+    }
+}
---
+++ sapxepmang/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace sapxepmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("a[{0}]: ",i);
+
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("offset: ");
+            int offset = int.Parse(Console.ReadLine());
+
+
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                int j = i - offset;
+
+                if (j >= n)
+                {
+                    j = j - n;
+                }
+                else if (j < 0){
+                    j = n + j;
+                }
+
+                b[i] = a[j];
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("{0}: ",b[i]);
+
+            }
+
+
+        }
+    }
+}
